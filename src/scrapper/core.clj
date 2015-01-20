3d2b1cb02ba3faf0d3d7c610b26a240c164b9ff4
tahmidsadik112 <tahmidsadik112@gmail.com>
2015-01-20 21:16:40 +0600

    Added CGrand's Enlive for templating and scraping

--- src/scrapper/core.clj
+++ src/scrapper/core.clj
@@ -1,7 +1,21 @@
 (ns scrapper.core
-  (:gen-class))
+  (:gen-class)
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.string :as str])
+  (:import (java.net URL)))
+(def base-url "http://www.lyrics71.com/eka-একা-2/")
+
+(defn fetch-url [url]
+  (html/html-resource (URL. url)))
+
+(def html-doc (fetch-url base-url))
+
+(defn get-lyric []
+  (map html/text (html/select html-doc [:div.content-post])))
+(def lyric (first (get-lyric)))
+(str/split lyric #"\n")
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (fetch-url base-url)))

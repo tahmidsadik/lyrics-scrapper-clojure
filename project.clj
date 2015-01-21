729353f049c11c3c9edcf3c4a52deaa01dce3474
tahmidsadik112 <tahmidsadik112@gmail.com>
2015-01-21 12:51:00 +0600

    Added Ring, Compojure, Hiccup, and cljjsbuild in order to make scrapping generic for l71.

--- project.clj
+++ project.clj
@@ -3,8 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2665"]
+                 [ring "1.3.2"]
+                 [compojure "1.3.2"]
+                 [hiccup "1.0.5"]
+                 [com.novemberain/monger "2.0.0"]
                  [enlive "1.1.5"]]
+
+  :plugins [[lein-cljsbuild "1.0.4"]
+            [lein-ring "0.9.1"]]
+
+  :ring {:handler scrapper.handler/handler}
+
+  :cljsbuild {:builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print false}}]}
+
   :main ^:skip-aot scrapper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
